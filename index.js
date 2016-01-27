49ce186841436e900bab06c8d4354ec8e9c20176
Ahmed Nasir <ahmednasir91@gmail.com>
2016-01-27 14:26:08 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 exports.gte = function(v1, v2) {
-	v1 = parseFloat(v1);
-	v2 = parseFloat(v2);
+	v1 = parseFloat(v1.replace("v", ""));
+	v2 = parseFloat(v2.replace("v", ""));
 
 	return v1 >= v2;
 };
