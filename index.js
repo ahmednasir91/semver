86940b55bb12b2b9a45d621d882213fb8ce93c1f
Ahmed Nasir <ahmednasir91@gmail.com>
2016-01-27 14:01:34 +0500

    Fixed function.

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-var Semver = {
-	gte: function(v1, v2) {
-		v1 = parseFloat(v1);
-		v2 = parseFloat(v2);
+exports.gte = function(v1, v2) {
+	v1 = parseFloat(v1);
+	v2 = parseFloat(v2);
 
-		return v1 >= v2;
-	}
-}
-
-exports.semver = Semver;
+	return v1 >= v2;
+};
